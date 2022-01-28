c1fe7931c32f864d32b04df8f3a2532096bfde68
zhaozc <zhaozicheng@rp-pet.cn>
2022-01-28 16:14:22 +0800

    docs: new-lang.js文件注释

--- build/bin/new-lang.js
+++ build/bin/new-lang.js
@@ -1,60 +1,64 @@
-'use strict';
+/*
+ * @Date: 2022-01-27 17:12:59
+ * @LastEditors: zhaozc
+ * @LastEditTime: 2022-01-28 15:17:06
+ * @FilePath: \ruipeng-ui\build\bin\new-lang.js
+ */
+'use strict'
+/**
+ * 为组件库添加新语言，比如 fr（法语）
+ *  分别为涉及到的文件（components.json、page.json、route.json、nav.config.json、docs）设置该语言的相关配置
+ *  具体的配置项默认为英语，你只需要在相应的文件中将这些英文配置项翻译为对应的语言即可
+ */
 
-console.log();
+console.log()
 process.on('exit', () => {
-  console.log();
-});
+    console.log()
+})
 
 if (!process.argv[2]) {
-  console.error('[language] is required!');
-  process.exit(1);
+    console.error('[language] is required!')
+    process.exit(1)
 }
 
-var fs = require('fs');
-const path = require('path');
-const fileSave = require('file-save');
-const lang = process.argv[2];
+var fs = require('fs')
+const path = require('path')
+const fileSave = require('file-save')
+const lang = process.argv[2]
 // const configPath = path.resolve(__dirname, '../../examples/i18n', lang);
 
 // 添加到 components.json
-const componentFile = require('../../examples/i18n/component.json');
+const componentFile = require('../../examples/i18n/component.json')
 if (componentFile.some(item => item.lang === lang)) {
-  console.error(`${lang} already exists.`);
-  process.exit(1);
+    console.error(`${lang} already exists.`)
+    process.exit(1)
 }
-let componentNew = Object.assign({}, componentFile.filter(item => item.lang === 'en-US')[0], { lang });
-componentFile.push(componentNew);
-fileSave(path.join(__dirname, '../../examples/i18n/component.json'))
-  .write(JSON.stringify(componentFile, null, '  '), 'utf8')
-  .end('\n');
+let componentNew = Object.assign({}, componentFile.filter(item => item.lang === 'en-US')[0], { lang })
+componentFile.push(componentNew)
+fileSave(path.join(__dirname, '../../examples/i18n/component.json')).write(JSON.stringify(componentFile, null, '  '), 'utf8').end('\n')
 
 // 添加到 page.json
-const pageFile = require('../../examples/i18n/page.json');
-let pageNew = Object.assign({}, pageFile.filter(item => item.lang === 'en-US')[0], { lang });
-pageFile.push(pageNew);
-fileSave(path.join(__dirname, '../../examples/i18n/page.json'))
-  .write(JSON.stringify(pageFile, null, '  '), 'utf8')
-  .end('\n');
+const pageFile = require('../../examples/i18n/page.json')
+// 新语言的默认配置为英语，你只需要去 page.json 中将该语言配置中的应为翻译为该语言即可
+let pageNew = Object.assign({}, pageFile.filter(item => item.lang === 'en-US')[0], { lang })
+pageFile.push(pageNew)
+fileSave(path.join(__dirname, '../../examples/i18n/page.json')).write(JSON.stringify(pageFile, null, '  '), 'utf8').end('\n')
 
 // 添加到 route.json
-const routeFile = require('../../examples/i18n/route.json');
-routeFile.push({ lang });
-fileSave(path.join(__dirname, '../../examples/i18n/route.json'))
-  .write(JSON.stringify(routeFile, null, '  '), 'utf8')
-  .end('\n');
+const routeFile = require('../../examples/i18n/route.json')
+routeFile.push({ lang })
+fileSave(path.join(__dirname, '../../examples/i18n/route.json')).write(JSON.stringify(routeFile, null, '  '), 'utf8').end('\n')
 
 // 添加到 nav.config.json
-const navFile = require('../../examples/nav.config.json');
-navFile[lang] = navFile['en-US'];
-fileSave(path.join(__dirname, '../../examples/nav.config.json'))
-  .write(JSON.stringify(navFile, null, '  '), 'utf8')
-  .end('\n');
+const navFile = require('../../examples/nav.config.json')
+navFile[lang] = navFile['en-US']
+fileSave(path.join(__dirname, '../../examples/nav.config.json')).write(JSON.stringify(navFile, null, '  '), 'utf8').end('\n')
 
 // docs 下新建对应文件夹
 try {
-  fs.statSync(path.resolve(__dirname, `../../examples/docs/${ lang }`));
+    fs.statSync(path.resolve(__dirname, `../../examples/docs/${lang}`))
 } catch (e) {
-  fs.mkdirSync(path.resolve(__dirname, `../../examples/docs/${ lang }`));
+    fs.mkdirSync(path.resolve(__dirname, `../../examples/docs/${lang}`))
 }
 
-console.log('DONE!');
+console.log('DONE!')
